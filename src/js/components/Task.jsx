eeeb86fe0d42bc0c32aa18f8b41a8d7e44a9c0df
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-04 19:03:13 +0000

    retouch styles

--- src/js/components/Task.jsx
+++ src/js/components/Task.jsx
@@ -5,7 +5,7 @@ const Task = ({taskName, handleDelete, id}) => {
         <div className='row'>
             <div className="col d-flex justify-content-center">
                 <div className='d-flex pt-3 border-top' style={{width:'70%'}}>
-                <span className='px-3' style={{minWidth:'300px'}}> <p >{taskName}</p></span> <button id={id} onClick={(e)=> handleDelete(e)} className={styles.taskBtn}>❌</button>
+                <span className='px-3' style={{minWidth:'300px'}}> <p >{taskName}</p></span> <button id={id} onClick={(e)=> handleDelete(e)} className={`${styles.taskBtn}`}>❌</button>
                 </div>
             </div>
         </div>
