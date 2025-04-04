069de3a250ab7510fba982d9f9148b30fc05fd51
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-04 19:22:52 +0000

    add hover styles

--- src/js/components/Task.jsx
+++ src/js/components/Task.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from '../../styles/button.module.css'
+
 const Task = ({taskName, handleDelete, id}) => {
     return (
         <div className='row'>
             <div className="col d-flex justify-content-center">
-                <div className='d-flex pt-3 border-top' style={{width:'70%'}}>
-                <span className='px-3' style={{minWidth:'300px'}}> <p >{taskName}</p></span> <button id={id} onClick={(e)=> handleDelete(e)} className={`${styles.taskBtn}`}>❌</button>
+                <div className={`${styles.taskCard} d-flex pt-3 border-top`} style={{width:'70%'}}>
+                    <span className='px-3' style={{minWidth:'300px'}}> <p >{taskName}</p></span> 
+                    <button id={id} onClick={(e)=> handleDelete(e)} className={`${styles.taskBtn}`}>❌</button>
                 </div>
             </div>
         </div>
