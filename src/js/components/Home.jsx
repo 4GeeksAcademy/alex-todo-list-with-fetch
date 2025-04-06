03298323eff5917c5b66d359dcfe06260eed6c92
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-06 19:53:56 +0000

    fix delay in taskCounter

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,87 +1,81 @@
 import React, { useEffect, useState } from "react";
-
-//include images into your bundle
 import Title from "./Title";
 import TasksContainer from "./TasksContainer";
 import TaskCounter from "./TaskCounter";
 
-const todoListUrl = 'https://playground.4geeks.com/todo'
-let  user;
-
+const todoListUrl = 'https://playground.4geeks.com/todo';
 
 const Home = () => {
-	const [tasks, setTasks] = useState([])
-	const [taskCounter, setTaskCounter] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [taskCounter, setTaskCounter] = useState(0);
 
-	const createTask = async (task) => {
-		const obj = {
-			method: 'POST',
-			body: JSON.stringify(task),
-			headers: {
-				"Content-Type": 'application/json'}
-		}
+  const createTask = async (task) => {
+    try {
+      await fetch(`${todoListUrl}/todos/alex`, {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      await getUser(`${todoListUrl}/users/alex`); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-		try {
-			await fetch(`${todoListUrl}/todos/alex`, obj)
-		} catch (error) {
-			console.error(error)	
-		}
-	}
+  const submitHandler = async (e) => { 
+    if (e.key === 'Enter') {
+      if (e.target.value === '') {
+        e.preventDefault();
+        return null;
+      }
+      e.preventDefault();
+      const newTask = {
+        label: e.target.value,
+        is_done: false,
+      };
+      await createTask(newTask);
+      e.target.value = '';
+    }
+  };
 
-	const submitHandler = (e) => {
-		if (e.key === 'Enter') {
-			if(e.target.value == '') {
-				e.preventDefault()
-				return null;
-			}
-			e.preventDefault()
-			const newTask = {
-				label: e.target.value, 
-				is_done: false, 
-			}
+  const getUser = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTasks(data.todos);
+      setTaskCounter(data.todos.length);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-			createTask(newTask)
-			setTaskCounter((prev)=> prev + 1)
-			e.target.value = ''
+  useEffect(() => {
+    getUser(`${todoListUrl}/users/alex`);
+  }, [taskCounter]); 
 
+  const deleteTask = async (id) => {
+    try {
+      await fetch(`${todoListUrl}/todos/${id}`, { method: 'DELETE' });
+      await getUser(`${todoListUrl}/users/alex`); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-		}
-	}
-	
-	const getUser = async (url) => {
-		try {
-			const response = await fetch(url)
-			const data = await response.json()
-			setTasks(data.todos)
-			setTaskCounter(data.todos.length)
-			
-		} catch (error) {
-			console.error(error)	
-		}
-	}
+  const handleDelete = async (e) => { 
+    const idToDelete = parseInt(e.target.id);
+    await deleteTask(idToDelete); 
+  };
 
-		useEffect(()=> {
-			getUser(`${todoListUrl}/users/alex`)
-	}, [taskCounter])
-
-	const deleteTask = async (id) => {
-		 await fetch(`${todoListUrl}/todos/${id}`,{method: 'DELETE'})
-		}
-
-	const handleDelete = (e) => {
-		setTaskCounter((prev)=> prev -1 )
-		deleteTask(e.target.id)
-		}
-
-
-	return (
-		<div className="container-fluid">
-			<Title/>
-			<TasksContainer submitHandler={submitHandler} handleDelete={handleDelete} tasks={tasks}/>
-			<TaskCounter taskCounter={taskCounter} />
-			
-        </div>
-	);
+  return (
+    <div className="container-fluid">
+      <Title />
+      <TasksContainer submitHandler={submitHandler} handleDelete={handleDelete} tasks={tasks} />
+      <TaskCounter taskCounter={taskCounter} />
+    </div>
+  );
 };
 
 export default Home;
